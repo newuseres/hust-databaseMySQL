dd69d62c53b0e1144fd7278ee3d041455cbaaba5
newuseres <simper@139.com>
2023-04-24 13:34:20 +0800

    完成3-15

---
+++ educoder-tasks/实训3 SELECT 之一/3-15.sql
@@ -0,0 +1,65 @@
+-- 15) 查询资产表中客户编号，客户基金投资总收益,基金投资总收益的排名(从高到低排名)。
+--     总收益相同时名次亦相同(即并列名次)。总收益命名为total_revenue, 名次命名为rank。
+--     第一条SQL语句实现全局名次不连续的排名，
+--     第二条SQL语句实现全局名次连续的排名。
+
+-- (1) 基金总收益排名(名次不连续)
+
+SELECT D.pro_c_id as pro_c_id,D.total_revenue as total_revenue,C.rk as 'rank'
+FROM (
+    SELECT A.total_revenue as total_revenue ,COUNT(B.total_revenue)+1 as rk
+    from (
+        SELECT DISTINCT sum( pro_income ) as total_revenue
+        FROM property
+        WHERE pro_type = 3 
+        GROUP BY pro_c_id
+    ) as A 
+    LEFT JOIN (
+        SELECT sum(  pro_income ) as total_revenue 
+        FROM property
+        WHERE pro_type = 3 
+        GROUP BY pro_c_id    
+    ) as B on A.total_revenue < B.total_revenue 
+    GROUP BY A.total_revenue
+)
+ as C 
+JOIN (
+    SELECT sum(  pro_income ) as total_revenue , pro_c_id
+    FROM property
+    WHERE pro_type = 3 
+    GROUP BY pro_c_id    
+) as D 
+on C.total_revenue = D.total_revenue
+ORDER BY C.rk ASC,pro_c_id;
+
+-- (2) 基金总收益排名(名次连续)
+SELECT D.pro_c_id as pro_c_id,D.total_revenue as total_revenue,C.rk as 'rank'
+FROM (
+    SELECT A.total_revenue as total_revenue ,COUNT(B.total_revenue)+1 as rk
+    from (
+        SELECT DISTINCT sum( pro_income ) as total_revenue
+        FROM property
+        WHERE pro_type = 3 
+        GROUP BY pro_c_id
+    ) as A 
+    LEFT JOIN (
+        SELECT DISTINCT  sum(  pro_income ) as total_revenue 
+        FROM property
+        WHERE pro_type = 3 
+        GROUP BY pro_c_id    
+    ) as B on A.total_revenue < B.total_revenue 
+    GROUP BY A.total_revenue
+)
+ as C 
+JOIN (
+    SELECT sum(  pro_income ) as total_revenue , pro_c_id
+    FROM property
+    WHERE pro_type = 3 
+    GROUP BY pro_c_id    
+) as D 
+on C.total_revenue = D.total_revenue
+ORDER BY C.rk ASC,pro_c_id;
+
+
+
+/*  end  of  your code  */
